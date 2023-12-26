4eed9753e35a4b3537be13e8ac08c8d8fa7641a3
Ikar <gewehrr@gmail.com>
2023-12-27 00:22:04 +0300

    first task

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,20 +1,43 @@
-﻿ int firstNumber, secondNumber;
- firstNumber == '5';
- secondNumber =='6';
- CompareNumbers;
-  
+﻿ 
+using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
  
- Console.Write firstNumber;
- int firsNumber = Convert.ToInt32(Console.ReadLine);
- Console.Write secondnumber;
- int secondNumber = Convert.ToInt32(Console.ReadLine);
- if (firsNumber > secondNumber)
+ 
+
+ 
+ if (firstNumber > secondNumber)
  {
-    Console.WriteLine("`5`");
+    Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
  }
-else secondNumber > firstNumber
+if(secondNumber > firstNumber)
 {
-    Console.WriteLine("'6'");
+    Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
 }
+if (secondNumber==firstNumber)
+{
+    Console.WriteLine($"Введенные числа равны `{firstNumber}` ");
+}
+ 
+    }
 
 
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 5;
+            secondNumber = 5;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
