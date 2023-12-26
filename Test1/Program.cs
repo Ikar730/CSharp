ad1e314c665be966cbdb5f8e5b9f027bb3dbad82
Ikar <gewehrr@gmail.com>
2023-12-26 22:45:26 +0300

    add Program.cs

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,2 +1,20 @@
 ﻿ int firstNumber, secondNumber;
+ firstNumber == '5';
+ secondNumber =='6';
+ CompareNumbers;
+  
  
+ Console.Write firstNumber;
+ int firsNumber = Convert.ToInt32(Console.ReadLine);
+ Console.Write secondnumber;
+ int secondNumber = Convert.ToInt32(Console.ReadLine);
+ if (firsNumber > secondNumber)
+ {
+    Console.WriteLine("`5`");
+ }
+else secondNumber > firstNumber
+{
+    Console.WriteLine("'6'");
+}
+
+
