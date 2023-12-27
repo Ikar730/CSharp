540223dbc78c99f5e1684c8569c1fc7f507fe619
Ikar <gewehrr@gmail.com>
2023-12-27 17:39:35 +0300

    temporary

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,43 +1,59 @@
 ﻿ 
+// using System;
+
+// public class Answer {
+//     static void CompareNumbers(int firstNumber, int secondNumber)
+//     {
+ 
+ 
+
+ 
+//  if (firstNumber > secondNumber)
+//  {
+//     Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+//  }
+// if(secondNumber > firstNumber)
+// {
+//     Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+// }
+// if (secondNumber==firstNumber)
+// {
+//     Console.WriteLine($"Введенные числа равны `{firstNumber}` ");
+// }
+ 
+//     }
 using System;
 
 public class Answer {
-    static void CompareNumbers(int firstNumber, int secondNumber)
+    
+  static int FindMax(int a, int b, int c)
     {
- 
- 
-
- 
- if (firstNumber > secondNumber)
- {
-    Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
- }
-if(secondNumber > firstNumber)
-{
-    Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
-}
-if (secondNumber==firstNumber)
-{
-    Console.WriteLine($"Введенные числа равны `{firstNumber}` ");
-}
- 
+int a = max;
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+int result = FindMax(a, b, c);
+        System.Console.WriteLine($"{result}");
+//  System.Console.WriteLine($"Число `{max}` является максимальным");
     }
+}
 
 
-  // Не удаляйте и не меняйте метод Main! 
-    static public void Main(string[] args) {
-        int firstNumber, secondNumber;
+//  Не удаляйте и не меняйте метод Main! 
+     static public void Main(string[] args) {
+         int firstNumber, secondNumber;
 
-        if (args.Length >= 2) {
-            firstNumber = int.Parse(args[0]);
-            secondNumber = int.Parse(args[1]);
-        } else {
-           // Здесь вы можете поменять значения для отправки кода на Выполнение
-            firstNumber = 5;
-            secondNumber = 5;
+         if (args.Length >= 2) {
+             firstNumber = int.Parse(args[0]);
+             secondNumber = int.Parse(args[1]);
+         } else {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+             firstNumber = 5;
+             secondNumber = 5;
         }
 
         // Не удаляйте строки ниже
         CompareNumbers(firstNumber, secondNumber);
-    }
-}
+     }
+ }             
