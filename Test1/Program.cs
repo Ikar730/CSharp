8993dd40fe1969b90b4421f1ef5c7b155e4590f2
Ikar <gewehrr@gmail.com>
2024-01-07 00:57:25 +0300

    метод,класс,поля

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -84,16 +84,16 @@
 
 //  Решение задач 2 семинар
 // проверяем кратность, введенного с клавиатуры числа
-int a=7;
-int b=23;
+// int a=7;
+// int b=23;
 
- Console.Write("Введите число : ");  //вывод текста на экран
-var line = Console.ReadLine();  //читаем строку с клавиатуры
-int number = Convert.ToInt32(line); //преобразование строки в число
+//  Console.Write("Введите число : ");  //вывод текста на экран
+// var line = Console.ReadLine();  //читаем строку с клавиатуры
+// int number = Convert.ToInt32(line); //преобразование строки в число
 
-if(number % a == 0 && number % b ==0)
-    Console.WriteLine(S"число {number} кратно {a} и {b}");
-else Console.WriteLine(S"число {number} некратно {a} и {b}");
+// if(number % a == 0 && number % b ==0)
+//     Console.WriteLine(S"число {number} кратно {a} и {b}");
+// else Console.WriteLine(S"число {number} некратно {a} и {b}");
 
 
 // в какой четверти системы координат (x,y), находится точка(если это не x=0 и y=0)
@@ -102,87 +102,125 @@ else Console.WriteLine(S"число {number} некратно {a} и {b}");
 // int c = ?;
 // int d = ?;
 // может через  int result
-Console.Write("\nВведите число x : " );
-var lineX = Console.ReadLine(); 
-int x = Convert.ToInt32(lineX);
-Console.Write("\nВведите число y : " );
-var lineY = Console.ReadLine(); 
-int y = Convert.ToInt32(lineY);
+// Console.Write("\nВведите число x : " );
+// var lineX = Console.ReadLine(); 
+// int x = Convert.ToInt32(lineX);
+// Console.Write("\nВведите число y : " );
+// var lineY = Console.ReadLine(); 
+// int y = Convert.ToInt32(lineY);
 
-if(x > 0 && y > 0)
-{
-Console.WriteLine($"точка ({x},{y}) находится в 1 четверти ");
-}
-if(x > 0 && y < 0)
-{
-Console.WriteLine($"точка ({x},{y}) находится в 4 четверти ");
-}
-if(x < 0 && y > 0)
-{
-Console.WriteLine($"точка ({x},{y}) находится в 2 четверти ");
-}
-if(x < 0 && y < 0)
-{
-Console.WriteLine($"точка ({x},{y}) находится в 3 четверти ");
-}
-
-
-Console.Write("Введите число : ");  
-var line = Console.ReadLine();  
-int number = Convert.ToInt32(line); 
-
-if (number < 10 || number > 99)
-{Console.WriteLine($"число {number} не подходит");
-return;}
-int x = number / 10;
-int y = number % 10;
-
-if(x >= y)
-Console.WriteLine($"цифра {x} числа {number} является наибольшей ");
-else
-Console.WriteLine($"цифра {y} числа {number} является наибольшей ");
+// if(x > 0 && y > 0)
+// {
+// Console.WriteLine($"точка ({x},{y}) находится в 1 четверти ");
+// }
+// if(x > 0 && y < 0)
+// {
+// Console.WriteLine($"точка ({x},{y}) находится в 4 четверти ");
+// }
+// if(x < 0 && y > 0)
+// {
+// Console.WriteLine($"точка ({x},{y}) находится в 2 четверти ");
+// }
+// if(x < 0 && y < 0)
+// {
+// Console.WriteLine($"точка ({x},{y}) находится в 3 четверти ");
+// }
 
 
-Console.Write("Введите число : ");  
-var line = Console.ReadLine();  
-int n = Convert.ToInt32(line);
+// Console.Write("Введите число : ");  
+// var line = Console.ReadLine();  
+// int number = Convert.ToInt32(line); 
 
-string stringNumber = n.ToString();
- for(var i = 0;i < number.Length; i++) 
-{
- Console.Write(stringNumber[i]);
- if (i < number.Length -1)
- Console.WriteLine (",");
-}
+// if (number < 10 || number > 99)
+// {Console.WriteLine($"число {number} не подходит");
+// return;}
+// int x = number / 10;
+// int y = number % 10;
+
+// if(x >= y)
+// Console.WriteLine($"цифра {x} числа {number} является наибольшей ");
+// else
+// Console.WriteLine($"цифра {y} числа {number} является наибольшей ");
+
+
+// Console.Write("Введите число : ");  
+// var line = Console.ReadLine();  
+// int n = Convert.ToInt32(line);
+
+// string stringNumber = n.ToString();
+//  for(var i = 0;i < number.Length; i++) 
+// {
+//  Console.Write(stringNumber[i]);
+//  if (i < number.Length -1)
+//  Console.WriteLine (",");
+// }
  
 
- Задача 4: Вывод цифр числа через запятую
+//  Задача 4: Вывод цифр числа через запятую
 using System;
 class Program
 {
+//Методы
+//  пример 1
+static int Sum(int a, int b) // метод принимает на вход А и Б
+{
+    return a + b; //  и возвращает их сумму
+}
+
+//  пример 2, метод, возвращающий строку
+static string GetString()
+{
+    return "эта строка будет возвращена";
+}
+
+// пример 3, принимающий число и строку, ничего не возвращающий
+
+static void PrintToConsole(int a, string message)
+{
+    Console.WriteLine (message + a);
+}
 static void Main()
+ {
+   Console.WriteLine(GetString()); 
+PrintToConsole(0,"test");
+// Console.Write("Введите натуральное число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 10)
+// {
+// Console.WriteLine(N);
+// }
+// else
+// {
+// while (N > 0)
+// {
+// int currentDigit = N % 10;
+// N /= 10;
+// if (N > 0)
+// {
+// Console.Write(currentDigit + ",");
+// }
+// else
+// {
+// Console.WriteLine(currentDigit);
+// }
+// }
+// }
+// }
+ }
+
+ 
+
+//  Класс (ссылочный тип)
+class CarClass
 {
-Console.Write("Введите натуральное число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-if (N < 10)
-{
-Console.WriteLine(N);
-}
-else
-{
-while (N > 0)
-{
-int currentDigit = N % 10;
-N /= 10;
-if (N > 0)
-{
-Console.Write(currentDigit + ",");
-}
-else
-{
-Console.WriteLine(currentDigit);
-}
-}
-}
+ int Weels{get;set;} //свойство, отличающееся от поля встроенными методами  get и set
+ int Wheight{get;set;}
+ int Doors{get;set;}
+ int Lights{get;set;}
+  int currentSpeed=0; //поле-обычная переменная внутри класса
+ public int GetSpeed() //метод, возвращающий текущую скорость
+ {
+    return currentSpeed;
+ }
 }
 }
