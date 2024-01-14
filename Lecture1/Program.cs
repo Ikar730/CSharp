431731b2134f39971600f94f56820d723ca6eac3
Ikar <gewehrr@gmail.com>
2024-01-14 20:54:19 +0300

    lecture1 redone

---
+++ Lecture1/Program.cs
@@ -0,0 +1,95 @@
+﻿//Task 1
+
+int value=100;
+ Console.WriteLine(value);
+
+ double d=5.55;
+ char c='!';
+ bool b=true;
+ Console.WriteLine(d);
+ Console.WriteLine(c);
+ Console.WriteLine(b);
+
+ //Task 2
+
+ int i1=5;
+int i2=6;
+int Sum=i1+i2;
+Console.WriteLine(Sum);
+
+long l1=6;
+long sum2=l1+i1;
+Console.WriteLine(Sum);
+
+double d1=5.5;
+double sum3=d1+i1;
+
+int product1=i1*i2;
+long product2=l1*i1;
+double product3=i1*d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+int quotient=i1/i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1/d1;
+Console.WriteLine(quotient2);
+
+//Task 3
+
+int a = 5;
+int b = 7;
+
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+//Task 4
+
+int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b>max);
+{
+    max=b;
+}
+if (c>max);
+{
+    max=c;
+}
+if (d>max);
+{
+    max=d;
+}
+if (e>max);
+{
+    max=e;
+}
+Console.WriteLine(max);
+
+//Task 5
+
+int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+   // Console.WriteLine(i);
+   Console.Write(i);
+    Console.Write(' ');
+
+    i = i + 1;
+}
