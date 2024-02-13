1fb306d3a38917f7ea1dffda120311c30e604da5
Ikar <gewehrr@gmail.com>
2024-02-13 18:02:07 +0300

    seminar2_modified

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -87,9 +87,103 @@
 // }
 // // 123 / 100 % 10 = 1  %  10 => 1
 
-int a = 14;
+//int a = 14;
 
-if (a % 7 == 0)
+//if (a % 7 == 0)
+//{
+//    Console.WriteLine($"Число {a} делится на 7 без остатка");
+//}
+
+
+//int FindFactorial(int n)
+//{
+//    // Базовый случай - случай остановки рекурсии
+//    if (n == 1) return 1;
+//    // Рекурсивный случай - вызов функции внутри функции 
+//    return n * FindFactorial(n - 1);
+//    //  FindFactorial(3)
+//    // 3 * 2 * FindFactorial(1) = 3 * 2 * 1
+//    // FindFactorial(2) = 2 * FindFactorial(1)
+//    // FindFactorial(1) = 1
+//}
+
+//Console.WriteLine(FindFactorial(4));
+
+
+//// // Напишите программу, которая будет принимать на вход число и
+//// // возвращать сумму его цифр.
+//// // Указание
+//// // Использовать рекурсию.
+//// // Пример
+//// // 123 => 6
+//// // 63 => 9 ( 6 + 3 )
+//int SumOfDigits(int number)
+//{
+//    if (number == 0) return 0; // Базовый случай
+//    int result = number % 10 + SumOfDigits(number / 10);
+//    return result;
+//    //  number = 63
+//    //  res = 63 % 10 + SumOfDigits(63/10); 
+//    //  res = 3 + 6 + 0 = 9
+//    //  SumOfDigits(6) = 6 + SumOfDigits(0); 
+//    //  SumOfDigits(0) = 0
+//}
+//Console.WriteLine(SumOfDigits(1006));
+
+
+//// // Рекурсия - вызов метода внутри метода
+//// // Факториал - про-е чисел от 1 до N включительно
+//// // 3! = 3 * 2 * 1= 6: n! = 1 * 2 ... * n
+//// n -> 1, 1 -> n?
+//int FindFactorial(int start)
+//{
+//    int n = 4;
+//    // Базовый случай - случай остановки рекурсии
+//    if (start == n) return n; // 1 * 2  * 3 * 4
+//                              // Рекурсивный случай - вызов функции внутри функции 
+//    return start * FindFactorial(start + 1); // j--
+//                                             // FindFactorial(4) 
+//                                             // 4 * 3  * 2 * 1  =24
+//                                             // FindFactorial(3)  = 3  * 2 * FindFactorial(1)
+//                                             // FindFactorial(1) = 1
+//}
+//Console.WriteLine(FindFactorial(1));
+
+
+
+//Урок 7. Рекурсия
+//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+//    int m =4;
+//int n = 12;
+//int m = start;
+//int m = end;
+
+//void ShowNumbers(int m,int n)
+//{
+//    if (m == n)
+//    { 
+//    Console.Write(m);
+//        return;
+//}
+//    Console.Write(m+" ");
+//    ShowNumbers(m + 1, n);
+//}
+//ShowNumbers(4, 12);
+
+void ShowNumbers(int start, int end)
 {
-    Console.WriteLine($"Число {a} делится на 7 без остатка");
+    if(start == end)
+    {
+        Console.Write(start);
+        return;
+    }
+    Console.Write(start+ " ");
+    ShowNumbers(start+1, end);
 }
+ShowNumbers(1, 10);
